4ca4d889ae1c5e89037a57e4719031ed1e468c19
pvnguyen21@clc.fitus.edu.vn <126704701+nguyen30042003@users.noreply.github.com>
2024-03-27 09:17:09 +0700

    upload  code AnalystBudgetVM

---
+++ MyShopProject/ViewModel/AnalystBudgetVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyShopProject.ViewModel
+{
+    public class AnalystBudgetVM : BaseViewModel
+    {
+        public ICommand revenew_Click {  get; set; }
+        public ICommand profit_Click { get; set; }
+        public ICommand topProduct_Click { get; set; }
+        public ICommand quantityProduct_Click { get; set; } 
+
+
+    }
+}
